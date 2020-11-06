f96787551f436d110317ae22b7288722ed39b621
Fabio De Araujo <fabioaraujo76@gmail.com>
2020-11-06 23:49:53 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Hkdz9CDNnaIujEYxQTnzfjESLEsW6uPunKVy3QvTrzfSqw9lhIleC0ex3QTfskMpLFp85igcuTldpJA9T5qWw0c00aNEA8CRe',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
-    
