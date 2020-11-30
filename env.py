e1147c991fec0954c835a1d50e56585f5783fbe2
Fabio De Araujo <fabioaraujo76@gmail.com>
2020-11-30 15:49:10 +0000

    environment variables exposed, now updated and remoom github

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("SECRET_KEY", "&dr6vmr%9_f=yg&5-qwzkpl-qti!^6cz0_-_ws2#13qxs3_3&o")
-os.environ.setdefault("STRIPE_PUBLIC_KEY", "pk_test_51Hkdz9CDNnaIujEYxQTnzfjESLEsW6uPunKVy3QvTrzfSqw9lhIleC0ex3QTfskMpLFp85igcuTldpJA9T5qWw0c00aNEA8CRe")
-os.environ.setdefault("STRIPE_SECRET_KEY", "sk_test_51Hkdz9CDNnaIujEYTQ4oJbuOmwwvKqmo3PrS84mBB5QCssVPzrLRolkhEfnjWhr9SUQwrp6Xa7eDvprNZtgmFkyE00Uc1ZwIqn")
-os.environ.setdefault("STRIPE_WH_SECRET", "whsec_EBkQVKBZxYHPL2Ln3q33UpXCpnMq7CEG")
+os.environ.setdefault("SECRET_KEY", "=3!#4-v6#+414*_r%4(w)-w)o0os(r+e1qlx2xk56%$__$m#os")
+os.environ.setdefault("STRIPE_PUBLIC_KEY", "pk_test_51Hkdz9CDNnaIujEYptohtx6KL6jMdzamo4KdbT03x8jnIiZ9pd2jCSogTFTaxF06a5JDZVo0eaIAo11rZOkF4CAX00tWqJv8c8")
+os.environ.setdefault("STRIPE_SECRET_KEY", "sk_test_51Hkdz9CDNnaIujEY4IAI3kg5HpqvJ8fGvx3cgPzcKuFSZgZJv5vqoxItGkZD8pffVtuCDgN6oXcafrdlw036ZAcq00HTCJVvE5")
+os.environ.setdefault("STRIPE_WH_SECRET", "whsec_mwFlFzokoz4UTU767P2vaRe5Rp46hRnJ")
