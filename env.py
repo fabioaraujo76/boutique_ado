9ad667ff5ea431f833f45ef482e44ff5d896a33b
Fabio De Araujo <fabioaraujo76@gmail.com>
2020-11-30 16:00:43 +0000

    removed secret keys and update gitignore

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("SECRET_KEY", "=3!#4-v6#+414*_r%4(w)-w)o0os(r+e1qlx2xk56%$__$m#os")
-os.environ.setdefault("STRIPE_PUBLIC_KEY", "pk_test_51Hkdz9CDNnaIujEYptohtx6KL6jMdzamo4KdbT03x8jnIiZ9pd2jCSogTFTaxF06a5JDZVo0eaIAo11rZOkF4CAX00tWqJv8c8")
-os.environ.setdefault("STRIPE_SECRET_KEY", "sk_test_51Hkdz9CDNnaIujEY4IAI3kg5HpqvJ8fGvx3cgPzcKuFSZgZJv5vqoxItGkZD8pffVtuCDgN6oXcafrdlw036ZAcq00HTCJVvE5")
-os.environ.setdefault("STRIPE_WH_SECRET", "whsec_mwFlFzokoz4UTU767P2vaRe5Rp46hRnJ")
+os.environ.setdefault("SECRET_KEY", "test")
+os.environ.setdefault("STRIPE_PUBLIC_KEY", "test")
+os.environ.setdefault("STRIPE_SECRET_KEY", "test")
+os.environ.setdefault("STRIPE_WH_SECRET", "test")
