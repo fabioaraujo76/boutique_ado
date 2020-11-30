92e6436c3ec48326db5fd78134fb95923e837bd0
Fabio De Araujo <fabioaraujo76@gmail.com>
2020-11-30 16:11:54 +0000

    removed tracking from env file

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "test")
-os.environ.setdefault("STRIPE_PUBLIC_KEY", "test")
-os.environ.setdefault("STRIPE_SECRET_KEY", "test")
-os.environ.setdefault("STRIPE_WH_SECRET", "test")
